48cbbab4a66c4bafe914ba02c107e54601a60f22
CarolineDuffett <ckduffett@gmail.com>
2022-07-03 17:04:22 -0400

    cleaned up sessions controller

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -3,35 +3,16 @@ const express = require('express')
 const sessions = express.Router()
 const Users = require('../models/user.js')
 
-// sessions.get('/new', (req, res) => {
-//   //res.render('sessions/new.ejs', { currentUser: req.session.currentUser })
-//   res.render({ currentUser: req.session.currentUser })
-// })
-
+//Get current user
 sessions.get('/sessions', (req, res) => {
-  //res.render('sessions/new.ejs', { currentUser: req.session.currentUser })
-  //res.render({ currentUser: req.session.currentUser })
-  //console.log(req.session.currentUser);
   res.json({ currentUser: req.session.currentUser })
+  console.log('got user backend');
+  console.log(req.session);
 })
 
 
-
-// sessions.get('/sessions', (req, res) => {
-//   req.session.user ? res.status(200).send({loggedIn: true}) : res.status(200).send({loggedIn: false})
-// })
-
-
-
-
-// on sessions form submit (log in)
+//Login user
 sessions.post('/sessions', (req, res) => {
-  // console.log('req.headers');
-  // console.log(req.headers);
-  // console.log("res.headers");
-  // console.log(res.headers); //undefined
-  // console.log('res');
-  // console.log(res);
   Users.findOne({ username: req.body.username }, (err, foundUser) => {
     if (err) {
       console.log(err)
@@ -43,7 +24,6 @@ sessions.post('/sessions', (req, res) => {
     } else {
       if (bcrypt.compareSync(req.body.password, foundUser.password)) {
         req.session.currentUser = foundUser
-        //res.json('user found!') //without app won't set session
         console.log('user found');
         res.json(req.session.currentUser)
         console.log(req.session.currentUser);
@@ -55,6 +35,7 @@ sessions.post('/sessions', (req, res) => {
   })
 })
 
+//Logout user
 sessions.delete('/sessions', (req, res) => {
   req.session.destroy(() => {
     res.json('logged out')
